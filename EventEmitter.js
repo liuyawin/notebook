bbb85adea47ddf7d85c2efa99eac00c17cbbca7c
liuyawin <717200899@qq.com>
2018-09-12 20:09:46 +0800

    232

---
+++ EventEmitter.js
@@ -0,0 +1,52 @@
+function EventEmitter(){
+    this._eventList = [];
+}
+
+EventEmitter.prototype = {
+    constructor: EventEmitter,
+    on: function(name, fn){
+        if (!this._eventList[name]) {
+            this._eventList[name] = [];
+        }
+        this._eventList[name].push(fn);
+    },
+    once: function(name, fn){
+        fn.isOnce = true;
+        this.on(name, fn);
+    },
+    emit: function(){
+        var name = Array.prototype.shift.call(arguments);
+        var fns = this._eventList[name];
+        var onceArr = [];
+        if (!fns || fns.length === 0) {
+            return false;
+        }
+        for (var i = 0; i < fns.length; i++) {
+            var fn = fns[i];
+            fn.apply(this, arguments);
+            if (fn.isOnce) {
+                onceArr.push(i);
+            }
+        }
+
+        for (var j = onceArr.length; j > 0 ; j--) {
+            fns.splice(onceArr[j], 1);
+        }
+    },
+    remove: function(name, fn){
+        var fns = this._eventList[name];
+        if (!fns) {
+            return false;
+        }
+        if (!fn) {
+            fns.length = 0;
+        } else {
+            for (var i = 0; i < fns.length; i++) {
+                var _fn = fns[i];
+                if (_fn === fn) {
+                    fns.splice(i, 1);
+                }
+            }
+        }
+    }
+}
